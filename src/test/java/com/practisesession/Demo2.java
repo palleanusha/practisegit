724d0973a65a7ea1869c948234c0eaae42738c6f
91934 <91934@LAPTOP-QDOHP8LB>
2021-12-15 10:12:35 +0530

    my practise project

---
+++ src/test/java/com/practisesession/Demo2.java
@@ -0,0 +1,13 @@
+package com.practisesession;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my project");
+	}
+
+}
+
+
+
